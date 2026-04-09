1e087c259b57c720effd1f0a147216d2c986f16f
maheshgajjela221 <166375267+maheshgajjela221@users.noreply.github.com>
2026-04-09 12:08:07 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://backend:8081/api")
+      .then(res => res.text())
+      .then(data => setMessage(data))
+      .catch(() => setMessage("Backend not reachable"));
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>React Frontend</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
