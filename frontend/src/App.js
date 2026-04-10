9a4cf76f0ef5e9912f33aed477a35f4590d0f182
maheshgajjela221 <166375267+maheshgajjela221@users.noreply.github.com>
2026-04-10 12:34:49 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ function App() {
   const [message, setMessage] = useState("");
 
   useEffect(() => {
-    fetch("http://54.152.180.73:8081/api")
+    fetch("/api")
       .then(res => res.text())
       .then(data => setMessage(data))
       .catch(() => setMessage("Backend not reachable"));
